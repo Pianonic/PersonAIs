82036e93bf07ca6fb8e60e43c43e72e4da9dcd68
PianoNic <79938743+Pianonic@users.noreply.github.com>
2024-09-24 20:35:13 +0200

    Frontent WIP

--- PersonAIs.Frontend/src/app/app.routes.ts
+++ PersonAIs.Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { EditorComponent } from './editor/editor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'editor', component: EditorComponent },
+    { path: '**', component: NotFoundComponent }
+];
