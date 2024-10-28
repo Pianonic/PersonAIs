9296948c324cbc29740b5027e306bc276c2971c6
PianoNic <79938743+Pianonic@users.noreply.github.com>
2024-10-28 16:43:29 +0100

    chore: Update project structure and remove unused files

--- PersonAIs.Frontend/src/app/app.routes.ts
+++ PersonAIs.Frontend/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { GeneratePersonaComponent } from './generate-persona/generate-persona.component';
-import { SettingsComponent } from './settings/settings.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
-    { path: '', component: ProjectsComponent }, // Default route
+    { path: 'projects', component: ProjectsComponent },
     { path: 'generate-persona', component: GeneratePersonaComponent },
-    { path: 'settings', component: SettingsComponent },
     { path: 'profile', component: ProfileComponent },
-    { path: '**', redirectTo: '' } // Redirect to home for unknown routes
+    { path: '**', redirectTo: '' }
   ];
